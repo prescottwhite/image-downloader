18976c6680ae38a45f79c7b507782bc29dfbae08
Prescott White <prgwhite@ucsc.edu>
2019-11-10 13:10:32 -0800

    Add click listeners to buttons in main (done on wrong branch)

--- app/src/main/java/com/cse118/imagedownloader/MainActivity.java
+++ app/src/main/java/com/cse118/imagedownloader/MainActivity.java
@@ -2,13 +2,47 @@ package com.cse118.imagedownloader;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mDownloadButton;
+    private Button mViewButton;
+    private Button mSearchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDownloadButton = findViewById(R.id.AM_button_download);
+        mDownloadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), DownloadActivity.class);
+                startActivity(intent);
+            }
+        });
+
+//        mViewButton = findViewById(R.id.AM_button_view);
+//        mViewButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(getBaseContext(), ViewActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+
+        mSearchButton = findViewById(R.id.AM_button_search);
+        mSearchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), SearchActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
